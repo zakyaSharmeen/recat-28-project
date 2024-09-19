c960c755e78eede5defba3228a1706c1f859fcc5
zakya sharmeen <sakarim9124@gmail.com>
2024-09-19 15:03:49 +0530

    context-api

---
+++ shopping-cart-context-api-04/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>hii zakya u have done this u just have to repeat this</h1>
+    </>
+  );
+}
+
+export default App;
