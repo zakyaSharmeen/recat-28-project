b19d9ee71c9a367a0d6588d08c81fb9b0e59c1d2
zakya sharmeen <sakarim9124@gmail.com>
2024-10-06 21:00:40 +0530

    practicedata

---
+++ crud-axious-13/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Practice from './practice-axious/Practice'
+
+function App() {
+  return (
+    <div>
+      <Practice/>
+    </div>
+  )
+}
+
+export default App
---
+++ crud-axious-13/src/practice-axious/Practice.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+const API = "https://jsonplaceholder.typicode.com";
+
+function Practice() {
+
+  const [myData, setMyData] = useState([])
+  const [error, setError] = useState("")
+
+
+
+  // using promises- .then
+  // useEffect(() =>{
+  //   axios
+  //   .get("https://jsonplaceholder.typicode.com/posts")
+  //   .then((res) => {
+  //     setMyData(res.data)
+
+      
+  //   })
+   
+  //   .catch((error) => {
+  //     setError(error.message)
+  //   });
+    
+  // },[])
+
+
+
+  // async await
+  // const getApiData =  (async () => {
+  //  try{
+  //   const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+  //   console.log(res.data);
+  // setMyData(res.data)  
+  //  }catch(error){
+  //   setError(error.message)
+  //  }  
+  // })
+
+  // useEffect(() =>{
+  //   getApiData()
+  // })
+
+
+  // effective method---by create a base api hold called const API 
+   const getApiData =  (async (url) => {
+   try{
+    const res = await axios.get(url)
+    console.log(res.data);
+  setMyData(res.data)  
+   }catch(error){
+    setError(error.message)
+   }  
+  })
+
+
+  useEffect(() =>{
+    getApiData(`${API}/posts`)
+  })
+
+
+  return (
+    <>
+    <h1>hii zakya u did u</h1>
+    {error !== "" && <h2>{error}</h2>}
+    .<div className='grid'>
+      {
+        myData.slice(0, 20).map((post) =>{
+
+          const {id, title,body} = post
+
+
+          return(
+              <div key={id}>
+                <h2>{title}</h2>
+                <p>{body}</p>
+              </div>
+          )
+        })
+      }
+      
+    </div>
+    </>
+  );
+}
+
+export default Practice;
