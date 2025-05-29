fdcf05dd0be0adcfb2410efce443cf0f68ba72dc
zakya sharmeen <sakarim9124@gmail.com>
2025-05-29 13:44:45 +0530

    added

--- movie-app-rtk-25/src/components/Header/Header.jsx
+++ movie-app-rtk-25/src/components/Header/Header.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import user from "../../images/user.png"
-import "./Header.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../../images/user.png";
+import "./Header.scss";
 
 function Header() {
   return (
     <div className="header">
-    <Link to="/home">
-      <div className="logo">Movie App</div>
-    </Link>
-    <div className="user-image">
-      <img src={user} alt="user" />
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img src={user} alt="user" />
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
